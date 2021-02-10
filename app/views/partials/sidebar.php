3d65c2ec390eef694437eb814c56db56a69eaa1e
rifjan29 <rjndla@gmail.com>
2021-02-10 14:34:43 +0800

    Constanta.php

--- app/views/partials/sidebar.php
+++ app/views/partials/sidebar.php
@@ -13,7 +13,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
             <li class="nav-item active">
-              <a class="nav-link" href="<?=base_url?>about/">Mahasiswa <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="<?=base?>about/">Mahasiswa <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Link</a>
--- app/views/partials/topbar.php
+++ app/views/partials/topbar.php
@@ -4,7 +4,7 @@
     <div class="bg-dark border-right" id="sidebar-wrapper">
       <div class="sidebar-heading bg-light">Absensi</div>
       <div class="list-group list-group-flush ">
-        <a href="<?=base_url?>home/" class="list-group-item list-group-item-action bg-dark text-white">Dashboard</a>
+        <a href="<?=base?>" class="list-group-item list-group-item-action bg-dark text-white">Dashboard</a>
         <a class="list-group-item list-group-item-action bg-dark text-white"  data-toggle="collapse" data-target="#demo">Siswa</a>
         <div id="demo" class="collapse">
           <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Data Siswa</a>
