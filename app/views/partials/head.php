7d1b27da5401d964c87ec2fae0856e2481b56755
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2021-02-12 17:27:59 +0700

    Update head.php

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?=base?>public/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?=base?>public/css/bootstrap.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=base?>public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=base?>public/css/bootstrap.css">
     <link rel="stylesheet" href="<?=base?>public/css/dataTables.bootstrap4.min.css">
     
     <!-- css -->
