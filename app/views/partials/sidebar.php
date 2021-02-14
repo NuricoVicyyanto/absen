c0285e194b2a6b8cabdfea071e8b3ff969aefa2a
rifjan29 <rjndla@gmail.com>
2021-02-15 00:17:27 +0800

    Tampilan Dashboard

--- app/views/partials/sidebar.php
+++ app/views/partials/sidebar.php
@@ -4,7 +4,7 @@
     <div id="page-content-wrapper">
 
       <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <button class="btn btn-primary" id="menu-toggle"><i class="fas fa-bars"></i></button>
+        <button class="btn btn-outline-secondary" id="menu-toggle"><i class="fas fa-bars"></i></button>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -12,25 +12,9 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="<?=base?>about/">Siswa <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="<?=base?>guru/">Guru <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
+           
+            <li class="nav-item d-flex justify-content-end">
+            <button type="button" class="btn btn-outline-danger">Keluar</button>
             </li>
           </ul>
         </div>
--- app/views/partials/topbar.php
+++ app/views/partials/topbar.php
@@ -1,15 +1,37 @@
     
   <div class="d-flex" id="wrapper">
     <!-- Sidebar -->
-    <div class="bg-dark border-right" id="sidebar-wrapper">
-      <div class="sidebar-heading bg-light">Absensi</div>
+    <div class="border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading border-bottom bg-light">Absensi Siswa</div>
       <div class="list-group list-group-flush ">
-        <a href="<?=base?>" class="list-group-item list-group-item-action bg-dark text-white">Dashboard</a>
-        <a class="list-group-item list-group-item-action bg-dark text-white"  data-toggle="collapse" data-target="#demo">Siswa</a>
-        <div id="demo" class="collapse">
-          <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Data Siswa</a>
-          <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Data Kelas</a>
+        <div class="list-group-item d-flex justify-content-center ">
+
+          <img src="<?=base?>public/img/user.jpg" alt="" class="img-thumbnail w-50 " >
+              <div class="dropdown">
+                <a class="dropdown-toggle p-2"
+                        type="button" id="dropdownMenu1" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false" style="font-size:12px">
+                        Rifjan Jundila
+                </a>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                  <a class="dropdown-item" href="#!">Action</a>
+                  <a class="dropdown-item" href="#!">Another action</a>
+                </div>
+              </div>
         </div>
+        <a href="<?=base?>" class="list-group-item list-group-item-action"><i class="fas fa-tachometer-alt mr-2 dash"></i>Dashboard</a>
+        <a href="<?=base?>" class="list-group-item list-group-item-action"><i class="far fa-clock mr-2 absensi"></i>Absensi</a>
+        <a href="<?=base?>" class="list-group-item list-group-item-action"><i class="fas fa-users mr-2 pengguna"></i>Pengguna</a>
+        <a href="<?=base?>" class="list-group-item list-group-item-action"><i class="fas fa-user-graduate mr-2 siswa"></i> Data Siswa</a>
+        <a href="<?=base?>" class="list-group-item list-group-item-action"><i class="far fa-user mr-2 guru"></i> Data Guru</a>
+        <a href="<?=base?>" class="list-group-item list-group-item-action"><i class="fas fa-graduation-cap mr-2 kelas"></i>Data Kelas</a>
+        <a href="<?=base?>" class="list-group-item list-group-item-action"><i class="fas fa-book mr-2 mapel"></i>Data Mapel</a>
+        <a href="<?=base?>" class="list-group-item list-group-item-action"><i class="fas fa-clipboard-list mr-2 laporan"></i>Laporan</a>
+        <!-- <a class="list-group-item list-group-item-action"  data-toggle="collapse" data-target="#demo">Siswa</a>
+        <div id="demo" class="collapse">
+          <a href="#" class="list-group-item list-group-item-action">Data Siswa</a>
+          <a href="#" class="list-group-item list-group-item-action">Data Kelas</a>
+        </div> -->
       </div>
     </div>
     <!-- /#sidebar-wrapper -->
