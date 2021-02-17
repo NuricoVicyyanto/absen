6cbe94c2045305101253b4ac56b19f13231ef770
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2021-02-17 08:41:04 +0700

    Update index.php

--- app/views/mapel/index.php
+++ app/views/mapel/index.php
@@ -1,28 +1,44 @@
-<br>
-<div class="col-lg-12">
-  <div class="shadow p-3 mb-5 bg-white rounded">
-    <nav class="navbar navbar-light bg-white">
-      <form class="form-inline">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </nav>
+    <div class="container-fluid mt-4">
+      <div class="row">
+        <div class="col-lg-12">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item active" aria-current="page"><i class="fas fa-book m-1"></i>Data
+                Mata Pelajaran</li>
+            </ol>
+          </nav>
+        </div>
+        <div class="col-lg-12">
+          <div class="shadow p-3 mb-5 bg-white rounded">
+            <nav class="navbar navbar-light bg-white">
+              <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </nav>
 
-    <div class="col-lg-12">
-      <div class="shadow p-3 mb-5 bg-white rounded">
-        <div class="table">
-          <table class="table" id="example">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-              </tr>
-            </thead>
-            <tbody>
+            <div class="col-lg-12">
+              <div class="shadow p-3 mb-5 bg-white rounded">
+                <div class="table">
+                  <table class="table" id="example">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                      </tr>
+                    </thead>
+                    <tbody>
 
-            </tbody>
-          </table>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+    <!-- /#page-content-wrapper -->
+    </div>
+    <!-- /#wrapper -->
